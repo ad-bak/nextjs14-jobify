e8baa4ea3e64f3fdbf5e3530bbc4fdc8c727f148
Adnan <adbak.dev@gmail.com>
2024-06-16 22:40:59 +0200

    action stats

--- utils/actions.ts
+++ utils/actions.ts
@@ -134,3 +134,36 @@ export async function updateJobAction(id: string, values: CreateAndEditJobType):
     return null;
   }
 }
+
+export async function getStatsAction(): Promise<{
+  pending: number;
+  interview: number;
+  declined: number;
+}> {
+  const userId = authenticateAndRedirect();
+  try {
+    const stats = await prisma.job.groupBy({
+      by: ["status"],
+      _count: {
+        status: true,
+      },
+      where: {
+        clerkId: userId,
+      },
+    });
+    const statsObject = stats.reduce((acc, curr) => {
+      acc[curr.status] = curr._count.status;
+      return acc;
+    }, {} as Record<string, number>);
+
+    const defaultStats = {
+      pending: 0,
+      declined: 0,
+      interview: 0,
+      ...statsObject,
+    };
+    return defaultStats;
+  } catch (error) {
+    redirect("/jobs");
+  }
+}
